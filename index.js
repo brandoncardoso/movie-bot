bb69b642c6f4a3fdf29c1d2b7ab71c6b69d902da
Brandon Cardoso <cardosobrandon@gmail.com>
2022-01-11 22:05:58 -0500

    check r/movies every hour for trailer, send to registered channels

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require('dotenv').config()
 const Discord = require('discord.io')
 const YoutubeSearch = require('ytsr')
+const schedule = require('node-schedule')
+const snoowrap = require('snoowrap')
 
 const COMMAND_PREFIX = '!'
+
+const NewTrailerChannels = []
+
+const reddit = new snoowrap({
+  userAgent: 'node:com.brandoncardoso.movie-trailer-bot:v1.0',
+  clientId: process.env.REDDIT_API_CLIENTID,
+  clientSecret: process.env.REDDIT_API_SECRET,
+  refreshToken: process.env.REDDIT_API_REFRESH_TOKEN,
+})
 
 var client = new Discord.Client({
   token: process.env.TOKEN,
@@ -11,7 +22,41 @@ var client = new Discord.Client({
 
 client.on('ready', () => {
   console.log(`logged in as ${client.username} - ${client.id}`)
+  schedule.scheduleJob('0 * * * *', checkMovieSubreddit)
 })
+
+async function checkMovieSubreddit() {
+  const rMoviesPosts = await reddit
+    .getSubreddit('movies')
+    .getHot({ limit: 10 }, 'hour')
+    .filter((post) => {
+      const title = post.title?.toLowerCase()
+      const flair = post.link_flair_text?.toLowerCase()
+
+      return (
+        !post.liked && // use reddit upvotes to track if trailer was seen by bot already
+        (title?.includes('trailer') || flair?.includes('trailer'))
+      )
+    })
+    .forEach((post) => {
+      post.upvote()
+      broadcastNewTrailer(post)
+    })
+}
+
+function broadcastNewTrailer(redditPost) {
+  const embeddedMessage = {
+    title: redditPost.title,
+    url: 'reddit.com' + redditPost.permalink,
+  }
+
+  NewTrailerChannels.forEach(async (channel) => {
+    client.sendMessage({
+      to: channel,
+      message: redditPost.url,
+    })
+  })
+}
 
 client.on('message', async (user, userId, channelID, message, event) => {
   if (userId === client.id) return
@@ -22,6 +67,12 @@ client.on('message', async (user, userId, channelID, message, event) => {
   switch (command) {
     case 'trailer':
       await handleTrailerCommand(channelID, arg)
+      break
+    case 'registertrailerchannel':
+      registerNewTrailerChannel(channelID)
+      break
+    case 'deregistertrailerchannel':
+      deregisterNewTrailerChannel(channelID)
       break
   }
 })
@@ -64,4 +115,27 @@ async function handleTrailerCommand(channelId, arg) {
       message: `I couldn't find a movie trailer for "${arg}".`,
     })
   }
+}
+
+function registerNewTrailerChannel(channelId) {
+  if (!NewTrailerChannels.find((ntc) => ntc === channelId)) {
+    NewTrailerChannels.push(channelId)
+  }
+
+  client.sendMessage({
+    to: channelId,
+    message: 'This channel is now registered to receive new movie trailers!',
+  })
+}
+
+function deregisterNewTrailerChannel(channelId) {
+  const index = NewTrailerChannels.indexOf(channelId)
+  if (index >= 0) {
+    NewTrailerChannels.splice(index, 1)
+  }
+
+  client.sendMessage({
+    to: channelId,
+    message: 'This channel will no longer receive new movie trailers.',
+  })
 }
